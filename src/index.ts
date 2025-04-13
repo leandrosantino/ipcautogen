3b5e98fbe9b27d3eb9c8b93140c7d832fad4f159
leandrosantino <leandrosantino2013@gmail.com>
2025-04-13 00:46:52 -0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { ipcMain } from "electron"
+
+export function defineIpcHandle(target: object, key: string, descriptor: PropertyDescriptor) {
+  if (!ipcMain) return;
+  ipcMain.handle(key, async (_, ...args) => {
+    return await descriptor.value.apply(target, args)
+  })
+}
+
+
+export function IpcChannel() {
+  return defineIpcHandle
+}
